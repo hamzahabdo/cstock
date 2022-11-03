b3c4025df1d4b4ebb2a369f6579960f03e94b371
hamzahabdo <alhamza1997@gmail.com>
2022-11-03 09:48:02 +0300

    Move:Fetish Common Stock Setting Doctype

---
+++ custom_stock/custom_stock/doctype/common_stock_setting/common_stock_setting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, burjalmaha and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Common Stock Setting', {
+	// refresh: function(frm) {
+
+	// }
+});
