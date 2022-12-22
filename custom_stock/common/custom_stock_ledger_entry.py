530f0925427bfc9a9617de9fecf15f5c4eb71866
rashed al qadi <rashedalqadi@rasheds-MacBook-Pro.local>
2022-12-22 16:34:43 +0300

    fix bug in custom dtock ledger entry

--- custom_stock/common/custom_stock_ledger_entry.py
+++ custom_stock/common/custom_stock_ledger_entry.py
@@ -6,7 +6,7 @@ from erpnext.stock.stock_ledger import NegativeStockError, get_previous_sle
 
 
 def check_actual_qty(doc, method):
-    if doc.voucher_type not in  ["Purchase Receipt","Stock Reconciliation"]:
+    if doc.voucher_type not in  ["Purchase Receipt","Stock Reconciliation","Sales Invoice"]:
         allow_negative_stock = frappe.get_all("Warehouse", filters={
                                             "name": doc.warehouse}, fields=["_allow_negative_stock"])[0]
 
@@ -25,6 +25,8 @@ def check_actual_qty(doc, method):
 
         # validate qty during submit
         if abs(flt((doc.actual_qty))) > flt(qty_after_transaction):
-
+            print("Hello World")
+            print("Hello World")
+            print("Hello World")
             frappe.throw(_("Row {0}: Quantity not available for {3} in warehouse {1} at posting time of the entry ({2})").format(doc.idx, frappe.bold(doc.warehouse), format_time(doc.posting_time), frappe.bold(doc.item_code))
                         + '<br><br>' + _("Available quantity is {0}, you need {1}").format(frappe.bold(doc.actual_qty), frappe.bold(qty_after_transaction)), NegativeStockError, title=_('Insufficient Stock'))
